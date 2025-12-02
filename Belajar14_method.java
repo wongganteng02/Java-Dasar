90a9c57db5355a54a3428a28e2cb6a474d5e226e
wongganteng02 <wongganteng0212@gmail.com>
2025-12-03 00:23:12 +0700

    Update Belajar14_method.java

--- Belajar14_method.java
+++ Belajar14_method.java
@@ -1,8 +1,15 @@
-package belajar;
-
-public class method {
+public class Belajar14_method {
   
-  static void hello() {
+  /* Fungsi static Adalah Agar Method Yg Bernama hello() Bisa Dipanggil Langsung Dari 
+  Method main Tanpa Membuat Objek.
+  
+     Fungsi void Adalah Agar Method Yg Bernama hello() Tidak Bisa Mengembalikan Nilai,  
+  Jika Method Punya Tipe-Data Maka Nilainya Tidak Bisa Dikembalikan Ke Program Utama / Method main.
+  
+     Fungsi heello() Adalah Jika Parameter Tidak Di isi Maka Berfungsi Sebagai Pemanggilan 
+  Fungsi Biasa Di Method main Yg Bisa Digunakan Berulang-ulang Tanpa Merubah Output. */
+  
+  static void hello() { 
     
     System.out.println("Hello Wong Ganteng");
     
