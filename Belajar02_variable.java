c9581f396346c5832045713f2207c746916010c9
Der Kusuma <wongganteng0212@gmail.com>
2025-12-06 13:14:20 +0700

    [upload]

---
+++ Belajar02_variable.java
@@ -0,0 +1,25 @@
+public class Belajar02_variable {
+
+    public static void main(String[] args){
+
+        // Membuat variabel
+        // Tempat menyimpan nilai sesuai tipe-data
+        
+        // Assigment adalah variable yg memiliki nilai yg sesuai dgn tipe-data
+        int a = 10; // Assignment
+        System.out.println("nilai a = " + a);
+
+        a = 20;
+        System.out.println("nilai a baru = " + a);
+
+        // Membuat sebuah deklarasi
+        // Sama seperti membuat variable tapi kosong tanpa nilai
+        int b; // Deklarasi
+
+        b = 7;
+
+        System.out.println("nilai b = " + b);
+
+
+    }
+}
